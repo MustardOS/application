881fff776215c1139cc674784f8a79269665af72
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2025-03-09 13:38:40 +1000

    Adjusted launch scripts to take advantage of Console Mode

--- PPSSPP/mux_launch.sh
+++ PPSSPP/mux_launch.sh
@@ -11,23 +11,33 @@ PPSSPP_DIR="$(GET_VAR "device" "storage/rom/mount")/MUOS/emulator/ppsspp"
 HOME="$PPSSPP_DIR"
 export HOME
 
-SDL_HQ_SCALER="$(GET_VAR "device" "sdl/scaler")"
-SDL_ROTATION="$(GET_VAR "device" "sdl/rotation")"
-SDL_BLITTER_DISABLED="$(GET_VAR "device" "sdl/blitter_disabled")"
+if [ "$(cat "$(GET_VAR "device" "screen/hdmi")")" -eq 1 ] && [ "$(GET_VAR "device" "board/hdmi")" -eq 1 ]; then
+	SDL_HQ_SCALER=2
+	SDL_ROTATION=0
+	SDL_BLITTER_DISABLED=1
+else
+	SDL_HQ_SCALER="$(GET_VAR "device" "sdl/scaler")"
+	SDL_ROTATION="$(GET_VAR "device" "sdl/rotation")"
+	SDL_BLITTER_DISABLED="$(GET_VAR "device" "sdl/blitter_disabled")"
+fi
+
 export SDL_HQ_SCALER SDL_ROTATION SDL_BLITTER_DISABLED
 
 cd "$PPSSPP_DIR" || exit
 
 SET_VAR "system" "foreground_process" "PPSSPP"
 
-[ "$(GET_VAR "device" "screen/rotate")" -eq 1 ] && touch "/tmp/ignore_double"
 FB_SWITCH 960 720 32
 
 sed -i '/^GraphicsBackend\|^FailedGraphicsBackends\|^DisabledGraphicsBackends/d' "$PPSSPP_DIR/.config/ppsspp/PSP/SYSTEM/ppsspp.ini"
 
 SDL_ASSERT=always_ignore SDL_GAMECONTROLLERCONFIG=$(grep "muOS-Keys" "/opt/muos/device/current/control/gamecontrollerdb_retro.txt") ./PPSSPP
 
-[ "$(GET_VAR "global" "settings/hdmi/enabled")" -eq 1 ] && SCREEN_TYPE="external" || SCREEN_TYPE="internal"
+SCREEN_TYPE="internal"
+if [ "$(cat "$(GET_VAR "device" "screen/hdmi")")" -eq 1 ] && [ "$(GET_VAR "device" "board/hdmi")" -eq 1 ]; then
+	SCREEN_TYPE="external"
+fi
+
 FB_SWITCH "$(GET_VAR "device" "screen/$SCREEN_TYPE/width")" "$(GET_VAR "device" "screen/$SCREEN_TYPE/height")" 32
 
 unset SDL_HQ_SCALER SDL_ROTATION SDL_BLITTER_DISABLED
