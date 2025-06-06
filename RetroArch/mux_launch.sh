8fef0d76b453425cb86c81af92f611489c56c0a1
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2025-06-06 15:46:15 +1000

    Fixed RetroArch loading from incorrect config path

--- RetroArch/mux_launch.sh
+++ RetroArch/mux_launch.sh
@@ -30,8 +30,8 @@ RA_CONF=/run/muos/storage/info/config/retroarch.cfg
 # retroarch.cfg will take precedence. Modified settings will save to the main
 # retroarch.cfg, not the included retroarch.device.cfg.)
 sed -n -e '/^#include /!p' \
-	-e '$a#include "/opt/muos/device/current/control/retroarch.device.cfg"' \
-	-e '$a#include "/opt/muos/device/current/control/retroarch.resolution.cfg"' \
+	-e '$a#include "/opt/muos/device/control/retroarch.device.cfg"' \
+	-e '$a#include "/opt/muos/device/control/retroarch.resolution.cfg"' \
 	-i "$RA_CONF"
 
 if [ "$(GET_VAR "kiosk" "content/retroarch")" -eq 1 ] 2>/dev/null; then
