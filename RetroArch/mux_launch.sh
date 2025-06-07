6d6134e488f141f169b060439fea9d898b1cb33a
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2025-06-07 18:07:07 +1000

    Moved SDL and RA setup to global functions

--- RetroArch/mux_launch.sh
+++ RetroArch/mux_launch.sh
@@ -10,35 +10,12 @@ echo app >/tmp/act_go
 HOME="$(GET_VAR "device" "board/home")"
 export HOME
 
-if [ "$(GET_VAR "config" "boot/device_mode")" -eq 1 ]; then
-	SDL_HQ_SCALER=2
-	SDL_ROTATION=0
-	SDL_BLITTER_DISABLED=1
-else
-	SDL_HQ_SCALER="$(GET_VAR "device" "sdl/scaler")"
-	SDL_ROTATION="$(GET_VAR "device" "sdl/rotation")"
-	SDL_BLITTER_DISABLED="$(GET_VAR "device" "sdl/blitter_disabled")"
-fi
-
-export SDL_HQ_SCALER SDL_ROTATION SDL_BLITTER_DISABLED
+SETUP_SDL_ENVIRONMENT
 
 SET_VAR "system" "foreground_process" "retroarch"
 
-RA_CONF=/run/muos/storage/info/config/retroarch.cfg
-
-# Include default button mappings from retroarch.device.cfg. (Settings in the
-# retroarch.cfg will take precedence. Modified settings will save to the main
-# retroarch.cfg, not the included retroarch.device.cfg.)
-sed -n -e '/^#include /!p' \
-	-e '$a#include "/opt/muos/device/control/retroarch.device.cfg"' \
-	-e '$a#include "/opt/muos/device/control/retroarch.resolution.cfg"' \
-	-i "$RA_CONF"
-
-if [ "$(GET_VAR "kiosk" "content/retroarch")" -eq 1 ] 2>/dev/null; then
-	sed -i 's/^kiosk_mode_enable = "false"$/kiosk_mode_enable = "true"/' "$RA_CONF"
-else
-	sed -i 's/^kiosk_mode_enable = "true"$/kiosk_mode_enable = "false"/' "$RA_CONF"
-fi
+RA_CONF="/run/muos/storage/info/config/retroarch.cfg"
+CONFIGURE_RETROARCH "$RA_CONF"
 
 nice --20 /usr/bin/retroarch -v -f -c "$RA_CONF"
 RA_PID=$!
